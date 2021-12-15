617d9c863ce385a0a4245c5262ba2e8218fcbc7e
Lunasphys <laurie.jeham@hotmail.fr>
2021-12-15 09:13:44 +0100

    site

--- FLTV-WebSite/src/app/streamers/streamers.component.ts
+++ FLTV-WebSite/src/app/streamers/streamers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-streamers',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StreamersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  streamers : any
+  index = 0
 
   ngOnInit(): void {
+    this.http.get('../../assets/streamers.json').subscribe(data =>{
+      this.streamers = data
+    })
+  }
+
+  previous() {
+    if (this.index == 0) {
+      this.index = this.streamers.length -1
+    } else {
+      this.index --
+    }
+    console.log(this.index)
+  }
+
+  next() {
+    if (this.index == this.streamers.length -1) {
+      this.index = 0
+    } else {
+      this.index ++
+    } 
+    console.log(this.index)
   }
 
 }

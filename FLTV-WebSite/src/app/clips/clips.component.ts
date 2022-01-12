5d846f1ba6acdb725060d94d877330d2cdd579a1
DJesusCry <lorenzo_98@hotmail.fr>
2022-01-12 09:07:34 +0100

    clips

--- FLTV-WebSite/src/app/clips/clips.component.ts
+++ FLTV-WebSite/src/app/clips/clips.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-clips',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClipsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer ) { }
+
+  url = "https://api.twitch.tv/helix/clips?broadcaster_id=737404029"
+  
+  headers = { 
+    'Client-Id': 'gmoua84o7ri07ene1n5wma4mn35z5s',
+    'Authorization' : 'Bearer gfxrobiw61o6cy2qahb6o1u2p1sgu9'
+  }
+  
+  clips: any = []
 
   ngOnInit(): void {
+
+    this.http.get(this.url, {headers:this.headers}).subscribe((data : any) => {
+      //console.log(data.data)
+      this.clips = data.data
+      console.log(this.clips)
+    })
+
   }
 
+
+  sanitizeURL(url : string){
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url)
+  }
 }

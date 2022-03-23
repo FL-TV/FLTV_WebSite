c842b91f5257306c5486b5618376608b3b56cdd1
DJesusCry <lorenzo_98@hotmail.fr>
2022-03-23 17:28:20 +0100

    streamers clean

--- FLTV-WebSite/src/app/streamers/streamers.component.ts
+++ FLTV-WebSite/src/app/streamers/streamers.component.ts
@@ -15,14 +15,14 @@ import { animate, style, transition, trigger } from '@angular/animations';
           [
             style({ opacity: 0 }),
             
-            animate('2s ease-in', style({ opacity: 1 }))
+            animate('1.5s ease-in', style({ opacity: 1 }))
 
           ]
         ),
         transition(
           ':leave',
           [
-            style({ opacity: 0 }),
+            style({ opacity: 0 })
             
 
           ]
